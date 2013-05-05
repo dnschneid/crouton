c2d6c420d9c98f86288f5087923e55137f860bcc
Nicolas Boichat <nicolas@boichat.ch>
2013-05-06 02:50:18 +0800

    Use xbindkeys instead of keylaunch: More flexibility for bindings.

---
+++ chroot-etc/xbindkeysrc.scm
@@ -0,0 +1,24 @@
+;; Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+;; Use of this source code is governed by a BSD-style license that can be
+;; found in the LICENSE file.
+
+;; Run xbindkeys -dg for some example configuration file with explanation
+
+; Xephyr-specific bindings
+(if (equal? (getenv "METHOD") "xephyr")
+    (begin
+        ; Replicates ratpoison shortcuts inside of Xephyr for when Xephyr grabs all keys
+        (xbindkey '(control shift alt F1) "xte 'keyup F1'; host-x11 ratpoison -c prev")
+        (xbindkey '(control shift alt F2) "xte 'keyup F2'; host-x11 ratpoison -c next")
+        (xbindkey '(control shift alt Escape) "xte 'keyup Escape'; host-x11 ratpoison -c 'readkey root'")
+    )
+)
+
+; X11-specific bindings
+(if (equal? (getenv "METHOD") "x11")
+    (begin
+        ; Creates the shortcuts to cycle X11 chroots like with Xephyr
+        (xbindkey '(control shift alt F1) "/usr/local/bin/croutoncycle prev")
+        (xbindkey '(control shift alt F2) "/usr/local/bin/croutoncycle prev")
+    )
+)
