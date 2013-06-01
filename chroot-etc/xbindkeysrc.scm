7f87c82b1b5983d32c44ba577c43c1f9ddf46017
Nicolas Boichat <nicolas@boichat.ch>
2013-06-01 17:21:42 +0800

    Map Alt+click to middle button (paste): useful for terminal.

--- chroot-etc/xbindkeysrc.scm
+++ chroot-etc/xbindkeysrc.scm
@@ -29,5 +29,9 @@
         ; Brightness control
         (xbindkey '(XF86MonBrightnessDown) "brightness down")
         (xbindkey '(XF86MonBrightnessUp) "brightness up")
+
+        ; Map Alt+click to middle button
+        ; Map on Release so that it does not appear both buttons are pressed
+        (xbindkey '(release alt "b:1") "xdotool click --clearmodifiers 2")
     )
 )
