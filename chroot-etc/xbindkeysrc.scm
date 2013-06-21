dd267ae662aa733928287f1eaf184e1f3c67f354
Nicolas Boichat <nicolas@boichat.ch>
2013-06-21 10:27:45 +0800

    Remove Alt+click binding. Instead, source ~/.xbindkeysrc.scm. + minor cleanups

--- chroot-etc/xbindkeysrc.scm
+++ chroot-etc/xbindkeysrc.scm
@@ -7,10 +7,14 @@
 ; Xephyr-specific bindings
 (if (equal? (getenv "METHOD") "xephyr")
     (begin
-        ; Replicates ratpoison shortcuts inside of Xephyr for when Xephyr grabs all keys
-        (xbindkey '(control shift alt F1) "xte 'keyup F1'; host-x11 ratpoison -c prev")
-        (xbindkey '(control shift alt F2) "xte 'keyup F2'; host-x11 ratpoison -c next")
-        (xbindkey '(control shift alt Escape) "xte 'keyup Escape'; host-x11 ratpoison -c 'readkey root'")
+        ; Replicates ratpoison shortcuts inside of Xephyr for when Xephyr
+        ; grabs all keys
+        (xbindkey '(control shift alt F1)
+            "xte 'keyup F1'; host-x11 ratpoison -c prev")
+        (xbindkey '(control shift alt F2)
+            "xte 'keyup F2'; host-x11 ratpoison -c next")
+        (xbindkey '(control shift alt Escape)
+            "xte 'keyup Escape'; host-x11 ratpoison -c 'readkey root'")
     )
 )
 
@@ -18,8 +22,8 @@
 (if (equal? (getenv "METHOD") "x11")
     (begin
         ; Creates the shortcuts to cycle X11 chroots like with Xephyr
-        (xbindkey '(control shift alt F1) "/usr/local/bin/croutoncycle prev")
-        (xbindkey '(control shift alt F2) "/usr/local/bin/croutoncycle prev")
+        (xbindkey '(control shift alt F1) "croutoncycle prev")
+        (xbindkey '(control shift alt F2) "croutoncycle next")
     )
 )
 
@@ -30,8 +34,8 @@
         (xbindkey '(XF86MonBrightnessDown) "brightness down")
         (xbindkey '(XF86MonBrightnessUp) "brightness up")
 
-        ; Map Alt+click to middle button
-        ; Map on Release so that it does not appear both buttons are pressed
-        (xbindkey '(release alt "b:1") "xdotool click --clearmodifiers 2")
+        (let ((usercfg (string-append (getenv "HOME") "/.xbindkeysrc.scm")))
+            (if (access? usercfg F_OK) (load usercfg))
+        )
     )
 )
