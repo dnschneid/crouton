b6937d97c77595349e8a4a41ce4730b962b712ff
David Schneider <dnschneid@chromium.org>
2013-09-30 13:57:06 -0700

    Detect and enforce the chroot architecture when updating

--- installer/ubuntu/getrelease.sh
+++ installer/ubuntu/getrelease.sh
@@ -3,23 +3,32 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# Grabs the release from the specified chroot ($1) and prints it out on stdout.
+# Grabs the release or arch from the specified chroot and prints it on stdout.
 # Fails with an error code of 1 if the chroot does not belong to this distro.
+# Returns 'incompatible' for the arch if it cannot be run on this system.
 
-if [ -z "$1" ]; then
-    echo "Usage: ${0##*/} chroot" 1>&2
+if [ "$#" != 2 ] || [ "$1" != '-a' -a "$1" != '-r' ]; then
+    echo "Usage: ${0##*/} -a|-r chroot" 1>&2
     exit 2
 fi
 
-lsb="${1%/}/etc/lsb-release"
-
-if ! grep -q 'DISTRIB_ID=Ubuntu' "$lsb" 2>/dev/null; then
+sources="${2%/}/etc/apt/sources.list"
+if [ ! -f "$sources" ]; then
     exit 1
 fi
 
-rel="`awk -F= '/^DISTRIB_CODENAME=/ {print $2; exit}' "$lsb"`"
-if [ -z "$rel" ]; then
+rel="`awk '/^deb /{print $3; exit}' "${2%/}/etc/apt/sources.list"`"
+if [ -z "$rel" ] || ! grep -q "^$rel[^a-z]*$" "`dirname "$0"`/releases"; then
     exit 1
+fi
+
+# Print the architecture if requested
+if [ "$1" = '-a' ]; then
+    if ! env -i chroot "$2" su -s '/bin/sh' \
+            -c 'dpkg --print-architecture' - root 2>/dev/null; then
+        echo 'incompatible'
+    fi
+    exit 0
 fi
 
 echo "$rel"
