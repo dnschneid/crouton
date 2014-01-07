a8b971c0e7fb0369d13eaa62824d8ad84abdb81a
David Schneider <dnschneid@chromium.org>
2014-01-07 14:38:56 -0800

    Release modifier keys before cycling.

--- chroot-etc/xbindkeysrc.scm
+++ chroot-etc/xbindkeysrc.scm
@@ -5,8 +5,18 @@
 ;; Run xbindkeys -dg for some example configuration file with explanation
 
 ; Cycle chroots
-(xbindkey '(control shift alt F1) "croutoncycle prev")
-(xbindkey '(control shift alt F2) "croutoncycle next")
+(xbindkey '(control shift alt F1) "
+    for key in Control Shift Alt Meta; do
+        echo 'keyup '$key'_L'
+        echo 'keyup '$key'_R'
+    done | xte
+    croutoncycle prev")
+(xbindkey '(control shift alt F2) "
+    for key in Control Shift Alt Meta; do
+        echo 'keyup '$key'_L'
+        echo 'keyup '$key'_R'
+    done | xte
+    croutoncycle next")
 
 ; Extra bindings that must only be activated in chroot X11/Xephyr
 (if (not (string-null? (getenv "XMETHOD")))
