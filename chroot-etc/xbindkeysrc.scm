e9b1155558a683b2f78f9da619f405466d25b528
David Schneider <dnschneid@chromium.org>
2014-01-13 20:17:07 -0800

    Work around more shortcut issues.

--- chroot-etc/xbindkeysrc.scm
+++ chroot-etc/xbindkeysrc.scm
@@ -5,8 +5,8 @@
 ;; Run xbindkeys -dg for some example configuration file with explanation
 
 ; Cycle chroots
-(xbindkey '(control shift alt F1) "croutoncycle prev")
-(xbindkey '(control shift alt F2) "croutoncycle next")
+(xbindkey '(control shift alt F1) "xte 'keyup F1'; croutoncycle prev")
+(xbindkey '(control shift alt F2) "xte 'keyup F2'; croutoncycle next")
 
 ; Extra bindings that must only be activated in chroot X11/Xephyr
 (if (not (string-null? (getenv "XMETHOD")))
