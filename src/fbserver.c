62aa41d805e835304f748e9ac5ebd7e709d16e8e
Christopher Mårtensson <cribalik@gmail.com>
2015-06-14 00:01:57 +0200

    Using access instead of stat for checking file existence

--- src/fbserver.c
+++ src/fbserver.c
@@ -492,12 +492,11 @@ int write_cursor() {
 }
 
 void write_init() {
-    struct stat stat_buf;
     char raw[FRAMEMAXHEADERSIZE + sizeof(struct initinfo)];
     struct initinfo* i = (struct initinfo*)(raw + FRAMEMAXHEADERSIZE);
     i->type = 'I';
     i->freon = 0;
-    if (stat("/sys/class/tty/tty0/active", &stat_buf) == -1) {
+    if (access("/sys/class/tty/tty0/active", F_OK) == -1) {
         trueorabort(errno == ENOENT, "Could not determine if using Freon or not");
         i->freon = 1;
     }
