8ced6c426ff1c336aa6c1855d8a567f9d310cf0f
Tony Xue <tonyxue@tonyxue.asia>
2015-06-29 10:03:06 +0800

    Improved lookup method

--- installer/ubuntu/getrelease.sh
+++ installer/ubuntu/getrelease.sh
@@ -18,11 +18,10 @@ if [ ! -s "$sources" ]; then
     exit 1
 fi
 
-rel="`awk '/^deb .* main( .*)?$/ \
-    { for ( i = 1; i <= NF; i++ ) \
-    { if ( $i~/http|file|cdrom|ftp|copy|rsh|ssh/ ) \
-    { i++; print $i; exit } } }' \
-    "$sources" "$sources.d"/*.list 2>/dev/null`"
+# Lookup the release name from the field after the URI
+# We identify URI by '://'
+rel="`sed -n 's|^deb .*://[^ ]* \([^ ]*\) main\( .*\)\?$|\1|p' \
+    "$sources" "$sources.d"/*.list | head -n 1`"
 if [ -z "$rel" ] || \
         ! grep -q "^$rel\([^a-z].*\)*$" "`dirname "$0"`/releases"; then
     exit 1
