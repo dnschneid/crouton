28e09e8f09360c662a41363d0ce0c8d37e546652
Joseph Jezak <josejx@gentoo.org>
2017-03-25 03:48:07 +0000

    Gentoo: Use CHOST from make.conf to determine architecture.

--- installer/gentoo/getrelease.sh
+++ installer/gentoo/getrelease.sh
@@ -13,15 +13,21 @@ if [ "$#" != 2 ] || [ "$1" != '-a' -a "$1" != '-r' ]; then
     exit 2
 fi
 
-# Check if this is gentoo
-if [ -f "${2%/}/etc/gentoo-release" ]; then
-    # Print the architecture if requested
-    if [ "$1" = '-a' ]; then
-        echo `readlink "${2%/}/etc/portage/make.profile" | cut -d"/" -f8`
-    else
-    	echo "gentoo"
-    fi
-    exit 0
-else
+# Check if this is gentoo by looking for the lsb release file
+if [ ! -f "${2%/}/etc/gentoo-release" ]; then
     exit 1
 fi
+
+# Get the architecture from the CHOST
+if [ "$1" = '-a' ]; then
+    # Get the CHOST architecture, first part of the tuple
+    CHOST=$(sed -n -e 's/^CHOST="\(.*\)*"/\1/p' "${2%/}/etc/portage/make.conf" | cut -d- -f1)
+
+    case "$CHOST" in
+    x86_64 | amd64) echo "amd64";;
+    *) echo "Invalid architecture '$ARCH'."; exit 2;;
+    esac
+else
+    echo "gentoo"
+fi
+exit 0
