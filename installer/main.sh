71b8888e27eb1816291dce50df19ca6d2a836be8
Nicolas Boichat <drinkcat@google.com>
2022-11-27 07:16:38 +0000

    Use ramfs instead of tmpfs
    Seems like Chromium OS >=107 blocked exec on tmpfs.

--- installer/main.sh
+++ installer/main.sh
@@ -625,9 +625,9 @@ if [ -z "$UPDATE$RESTOREBIN" ] && [ -n "$DOWNLOADONLY" -o -z "$TARBALL" ]; then
     subdir="$RELEASE-$ARCH"
     addtrap "rm -rf --one-file-system '$tmp'"
 
-    # Ensure that the temporary directory has exec+dev, or mount a new tmpfs
+    # Ensure that the temporary directory has exec+dev, or mount a new ramfs
     if [ "$NOEXECTMP" = 'y' ]; then
-        mount -i -t tmpfs -o 'rw,dev,exec' tmpfs "$tmp"
+        mount -i -t ramfs -o 'rw,dev,exec' ramfs "$tmp"
         # Ensure symfollow is set on platforms that feature it, but don't fail
         # on ones that do not.
         mount -o 'remount,symfollow' "$tmp" 2>/dev/null || true
